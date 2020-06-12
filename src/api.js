ca5adb56c83644278ae7bee0e81427959575e8cf
saygik <S@ygibon1>
2020-06-12 11:28:53 +0300

    change api base

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ import {avatarColors} from './services'
 axios.defaults.timeout = 30000
 
 //const _apiBase = 'http://vm-say-work.brnv.rw:9003'
-const _apiBase = 'http://ad-users.brnv.rw'
+//const _apiBase = 'http://ad-users.brnv.rw'
+const _apiBase = 'http://find-ad.brnvrw.by'
 const api = {};
 api.getAdUser=(setProgress)=>{
     return new Promise((resolve, reject) => {
